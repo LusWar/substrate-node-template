ccd379428f1cccb5b9349e651ea38e88333ff06e
luswar <luswar@163.com>
2020-11-15 08:13:31 +0800

    Add claim pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -41,6 +41,9 @@ pub use frame_support::{
 /// Import the template pallet.
 pub use pallet_template;
 
+/// Import the claim pallet.
+pub use pallet_claim;
+
 /// An index to a block.
 pub type BlockNumber = u32;
 
@@ -266,6 +269,13 @@ impl pallet_template::Trait for Runtime {
 	type Event = Event;
 }
 
+/// Configure the claim pallet in pallets/claim.
+impl pallet_claim::Trait for Runtime {
+	type Event = Event;
+	type Call = Call;
+	type Currency = Balances;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -283,6 +293,7 @@ construct_runtime!(
 		Sudo: pallet_sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the template pallet in the runtime.
 		TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+		ClaimModule: pallet_claim::{Module, Call, Storage, Event<T>, ValidateUnsigned},
 	}
 );
 
